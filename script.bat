5d80c59678bdeca3c0a2a57bfed80da59c4158c3
marisangila <marisangila.alves@edu.sc.senai.br>
2023-09-05 16:53:12 -0300

    folders

--- script.bat
+++ script.bat
@@ -1,105 +1,210 @@
 mkdir Analice
     cd Analice
     mkdir atividade4
-    mkdir atividade5
+    cd atividade4
+    type nul > .gitkeep
     cd ..
+    mkdir atividade5
+    cd atividade5
+    type nul > .gitkeep
+    cd ../../
 mkdir Brunno
     cd Brunno
     mkdir atividade4
+    cd atividade4
+    type nul > .gitkeep
+    cd ..
     mkdir atividade5
-    cd ..	
+    cd atividade5
+    type nul > .gitkeep
+    cd ../../	
 mkdir Carlos
     cd Carlos	
     mkdir atividade4
+    cd atividade4
+    type nul > .gitkeep
+    cd ..
     mkdir atividade5
-    cd ..	
+    cd atividade5
+    type nul > .gitkeep
+    cd ../../	
 mkdir Cauan
     cd Cauan
     mkdir atividade4
+    cd atividade4
+    type nul > .gitkeep
+    cd ..
     mkdir atividade5
-    cd ..		
+    cd atividade5
+    type nul > .gitkeep
+    cd ../../		
 mkdir Daniel
     cd Daniel	
     mkdir atividade4
+    cd atividade4
+    type nul > .gitkeep
+    cd ..
     mkdir atividade5
-    cd ..	
+    cd atividade5
+    type nul > .gitkeep
+    cd ../../	
 mkdir Eliel
     cd Eliel
     mkdir atividade4
+    cd atividade4
+    type nul > .gitkeep
+    cd ..
     mkdir atividade5
-    cd ..	
+    cd atividade5
+    type nul > .gitkeep
+    cd ../../	
 mkdir Eros
     cd Eros
     mkdir atividade4
+    cd atividade4
+    type nul > .gitkeep
+    cd ..
     mkdir atividade5
-    cd ..		
+    cd atividade5
+    type nul > .gitkeep
+    cd ../../		
 mkdir Fernanda
     cd Fernanda
     mkdir atividade4
+    cd atividade4
+    type nul > .gitkeep
+    cd ..
     mkdir atividade5
-    cd ..	
+    cd atividade5
+    type nul > .gitkeep
+    cd ../../	
 mkdir Fernando	
     cd Fernando
     mkdir atividade4
+    cd atividade4
+    type nul > .gitkeep
+    cd ..
     mkdir atividade5
-    cd ..	
+    cd atividade5
+    type nul > .gitkeep
+    cd ../../	
 mkdir Gabriel_Reis	
     cd Gabriel_Reis
     mkdir atividade4
+    cd atividade4
+    type nul > .gitkeep
+    cd ..
     mkdir atividade5
-    cd ..	
+    cd atividade5
+    type nul > .gitkeep
+    cd ../../	
 mkdir Gabriel_Soares
     cd Gabriel_Soares
     mkdir atividade4
+    cd atividade4
+    type nul > .gitkeep
+    cd ..
     mkdir atividade5
-    cd ..		
+    cd atividade5
+    type nul > .gitkeep
+    cd ../../		
 mkdir Gustavo
     cd Gustavo
     mkdir atividade4
+    cd atividade4
+    type nul > .gitkeep
+    cd ..
     mkdir atividade5
-    cd ..		
+    cd atividade5
+    type nul > .gitkeep
+    cd ../../		
 mkdir Joao_Viktor
     cd Joao_Viktor
     mkdir atividade4
+    cd atividade4
+    type nul > .gitkeep
+    cd ..
     mkdir atividade5
-    cd ..	
+    cd atividade5
+    type nul > .gitkeep
+    cd ../../	
 mkdir Leandro
     cd Leandro
     mkdir atividade4
+    cd atividade4
+    type nul > .gitkeep
+    cd ..
     mkdir atividade5
-    cd ..		
+    cd atividade5
+    type nul > .gitkeep
+    cd ../../		
 mkdir Natalia
     cd Natalia
     mkdir atividade4
+    cd atividade4
+    type nul > .gitkeep
+    cd ..
     mkdir atividade5
-    cd ..		
+    cd atividade5
+    type nul > .gitkeep
+    cd ../../		
 mkdir Nathallie
     cd Nathallie
     mkdir atividade4
+    cd atividade4
+    type nul > .gitkeep
+    cd ..
     mkdir atividade5
-    cd ..	
+    cd atividade5
+    type nul > .gitkeep
+    cd ../../	
 mkdir Pedro
     cd Pedro
     mkdir atividade4
+    cd atividade4
+    type nul > .gitkeep
+    cd ..
     mkdir atividade5
-    cd ..		
+    cd atividade5
+    type nul > .gitkeep
+    cd ../../		
 mkdir Ruan
     cd Ruan
     mkdir atividade4
+    cd atividade4
+    type nul > .gitkeep
+    cd ..
     mkdir atividade5
-    cd ..	
+    cd atividade5
+    type nul > .gitkeep
+    cd ../../	
 mkdir Sara
     cd Sara
     mkdir atividade4
+    cd atividade4
+    type nul > .gitkeep
+    cd ..
     mkdir atividade5
-    cd ..	
+    cd atividade5
+    type nul > .gitkeep
+    cd ../../	
 mkdir Thais
     cd Thais
     mkdir atividade4
+    cd atividade4
+    type nul > .gitkeep
+    cd ..
     mkdir atividade5
-    cd ..		
+    cd atividade5
+    type nul > .gitkeep
+    cd ../../			
 mkdir Thayna
     cd Thayna
     mkdir atividade4
+    cd atividade4
+    type nul > .gitkeep
+    cd ..
     mkdir atividade5
-    cd ..	
+    cd atividade5
+    type nul > .gitkeep
+    cd ../../	
