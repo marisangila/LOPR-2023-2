50443f999f9cb4cd12c901bc49f285631f943d79
marisangila <marisangila.alves@edu.sc.senai.br>
2023-09-05 16:24:08 -0300

    folders

---
+++ script.bat
@@ -0,0 +1,105 @@
+mkdir Analice
+    cd Analice
+    mkdir atividade4
+    mkdir atividade5
+    cd ..
+mkdir Brunno
+    cd Brunno
+    mkdir atividade4
+    mkdir atividade5
+    cd ..	
+mkdir Carlos
+    cd Carlos	
+    mkdir atividade4
+    mkdir atividade5
+    cd ..	
+mkdir Cauan
+    cd Cauan
+    mkdir atividade4
+    mkdir atividade5
+    cd ..		
+mkdir Daniel
+    cd Daniel	
+    mkdir atividade4
+    mkdir atividade5
+    cd ..	
+mkdir Eliel
+    cd Eliel
+    mkdir atividade4
+    mkdir atividade5
+    cd ..	
+mkdir Eros
+    cd Eros
+    mkdir atividade4
+    mkdir atividade5
+    cd ..		
+mkdir Fernanda
+    cd Fernanda
+    mkdir atividade4
+    mkdir atividade5
+    cd ..	
+mkdir Fernando	
+    cd Fernando
+    mkdir atividade4
+    mkdir atividade5
+    cd ..	
+mkdir Gabriel_Reis	
+    cd Gabriel_Reis
+    mkdir atividade4
+    mkdir atividade5
+    cd ..	
+mkdir Gabriel_Soares
+    cd Gabriel_Soares
+    mkdir atividade4
+    mkdir atividade5
+    cd ..		
+mkdir Gustavo
+    cd Gustavo
+    mkdir atividade4
+    mkdir atividade5
+    cd ..		
+mkdir Joao_Viktor
+    cd Joao_Viktor
+    mkdir atividade4
+    mkdir atividade5
+    cd ..	
+mkdir Leandro
+    cd Leandro
+    mkdir atividade4
+    mkdir atividade5
+    cd ..		
+mkdir Natalia
+    cd Natalia
+    mkdir atividade4
+    mkdir atividade5
+    cd ..		
+mkdir Nathallie
+    cd Nathallie
+    mkdir atividade4
+    mkdir atividade5
+    cd ..	
+mkdir Pedro
+    cd Pedro
+    mkdir atividade4
+    mkdir atividade5
+    cd ..		
+mkdir Ruan
+    cd Ruan
+    mkdir atividade4
+    mkdir atividade5
+    cd ..	
+mkdir Sara
+    cd Sara
+    mkdir atividade4
+    mkdir atividade5
+    cd ..	
+mkdir Thais
+    cd Thais
+    mkdir atividade4
+    mkdir atividade5
+    cd ..		
+mkdir Thayna
+    cd Thayna
+    mkdir atividade4
+    mkdir atividade5
+    cd ..	
