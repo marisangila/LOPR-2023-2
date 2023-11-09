c83dc2028dbdbed888f9434ef012d9717e95335b
daniwells <130805695+daniwells@users.noreply.github.com>
2023-11-09 13:51:52 -0300

    Realizando a atividade do quiz

---
+++ Cauan/atividade15/quiz/main.py
@@ -0,0 +1,32 @@
+#Código que gera um quiz onde até quatro jogadores podem participar
+#Pode existir um administrador, que dada uma senha pode produzir novas questões
+
+#AUTORES: Daniel e Cauan
+
+import os
+from Entidades import Questoes
+from Jogo import menu, quantidadeJogadores
+#=-------------------------------------------------------------=
+
+senha = "1234"
+
+#=---------------------------------------------------------------=
+#linha de código principal
+
+participantesDoJogo = quantidadeJogadores()
+
+questoes = Questoes()
+menu(participantesDoJogo, questoes, senha)
+
+os.system("clear")
+cont = 0
+print("-" * 30)
+print()
+for participante in participantesDoJogo:
+  print(
+      f"{participantesDoJogo[cont]['Nome'][0]}: \033[1;33m{participante['Nome'][2]} PONTOS\033[m"
+  )
+  cont += 1
+print()
+print("-" * 30)
+print("-- FIM DE JOGO --")
---
+++ Cauan/atividade15/quiz/readme.md
@@ -0,0 +1,1 @@
+Executar o programa no shell, ou em algum interpretador baseado em unix.
