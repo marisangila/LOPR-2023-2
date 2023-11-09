02a14a1ed876d7b6786082c3cb5f679e48098dd9
doka43 <cauan_moreira@estudante.sesisenai.org.br>
2023-11-09 16:56:50 -0300

    atividade17

---
+++ Cauan/atividade17/recursividade.java
@@ -0,0 +1,34 @@
+package atividade17;
+
+import java.util.Scanner;
+
+public class recursividade{
+
+    public static void main(String[] args){
+        int inicio = 0;
+        Scanner newNumber = new Scanner(System.in);
+        System.out.println("Digite um valor de n: ");
+
+        int aNumber = newNumber.nextInt();
+        int n = aNumber;
+
+        double result = inicio + calcularFracao(n);
+        System.out.println(result);
+    }
+
+    public static double calcularFracao(int n){
+        if (n == 1){
+            return 1.0;
+        }
+        else{
+            return 1.0/n + calcularFracao(n-1);
+        }
+    }
+
+
+
+
+
+
+
+}
